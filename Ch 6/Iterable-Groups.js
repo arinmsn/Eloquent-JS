e6af54c260101ad0b837d820ab86aca7b15ccfd8
arinmsn <arinmsn@gmail.com>
2019-10-03 16:37:06 -0700

    6.3 - Iterable Groups exercise

---
+++ Ch 6/Iterable-Groups.js
@@ -0,0 +1,92 @@
+/*
+    Instructions
+
+	 Make the Group class from the previous exercise iterable.
+	 Refer to the section about the iterator interface earlier 
+	 in the chapter if you aren’t clear on the exact form of
+	 the interface anymore.
+
+	If you used an array to represent the group’s members,
+	don’t just return the iterator created by calling the Symbol
+	iterator method on the array. That would work, but it
+	defeats the purpose of this exercise.
+
+	It is okay if your iterator behaves strangely when the group
+	is modified during iteration.
+
+	// Your code here (and the code from the previous exercise)
+
+	for (let value of Group.from(["a", "b", "c"])) {
+		console.log(value);
+	}
+	// → a
+	// → b
+	// → c
+
+	Hint:
+
+	It is probably worthwhile to define a new class GroupIterator.
+	Iterator instances should have a property that tracks the current
+	position in the group. Every time next is called, it checks
+	whether it is done and, if not, moves past the current value and returns it.
+
+	The Group class itself gets a method named by Symbol.iterator that,
+	when called, returns a new instance of the iterator class for that group.
+*/
+
+class Group {
+  constructor() {
+    this.members = [];
+  }
+
+  add(value) {
+    if (!this.has(value)) {
+      this.members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.members = this.members.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group();
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+
+  [Symbol.iterator]() {
+    return new GroupIterator(this);
+  }
+}
+
+class GroupIterator {
+  constructor(group) {
+    this.group = group;
+    this.position = 0;
+  }
+
+  next() {
+    if (this.position >= this.group.members.length) {
+      return { done: true };
+    } else {
+      let result = { value: this.group.members[this.position], done: false };
+      this.position++;
+      return result;
+    }
+  }
+}
+
+for (let value of Group.from(["a", "b", "c"])) {
+  console.log(value);
+}
+
+// a
+// b
+// c
