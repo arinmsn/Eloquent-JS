d7d4bb8022e4a80eb50e4b5a35374366e97a6043
arinmsn <arinmsn@gmail.com>
2019-10-04 21:36:30 -0700

    Borrowing A Method exercise solved

---
+++ Ch 6/Borrowing-A-Method.js
@@ -0,0 +1,28 @@
+/*
+Earlier in the chapter I mentioned that an object’s hasOwnProperty can be
+used as a more robust alternative to the in operator when you want to ignore
+the prototype’s properties. But what if your map needs to include the word
+"hasOwnProperty"? You won’t be able to call that method anymore because
+the object’s own property hides the method value.
+
+Can you think of a way to call hasOwnProperty on an object that
+has its own property by that name?
+
+
+*/
+
+let map = { one: true, two: true, hasOwnProperty: true };
+
+// Fix this call
+console.log(map.hasOwnProperty("one"));
+// → true
+
+/*  Hint:
+Remember that methods that exist on plain objects
+come from Object.prototype.
+Also remember that you can call a function with a
+specific this binding by using its call method.
+*/
+
+// Solution:
+console.log(Object.prototype.hasOwnProperty.call(map, "one"));
