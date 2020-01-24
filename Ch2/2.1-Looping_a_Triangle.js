d4e331ff77d97c5dc59b18eb538b388dfacbd424
arinmsn <arinmsn@gmail.com>
2020-01-23 18:51:06 -0800

    Ch2 - Exercises (2.1)

---
+++ Ch2/2.1-Looping_a_Triangle.js
@@ -0,0 +1,16 @@
+/*
+2.1 - Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+for (let line = "#"; line.length < 8; line += "#") {
+  console.log(line);
+}
