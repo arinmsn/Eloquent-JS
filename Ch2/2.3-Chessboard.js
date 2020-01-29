f5610b02db4a74dd88d779edd77cc57369a3c214
arinmsn <arinmsn@gmail.com>
2020-01-28 19:57:16 -0800

    Chessboard solution

---
+++ Ch2/2.3-Chessboard.js
@@ -0,0 +1,44 @@
+/*
+Chessboard
+Write a program that creates a string that represents an 8×8 grid,
+using newline characters to separate lines. At each position of the
+grid there is either a space or a "#" character.
+The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # # 
+ # # # #
+# # # #
+
+When you have a program that generates this pattern,
+define a binding size = 8 and change the program so
+that it works for any size, outputting a grid of the
+given width and height.
+
+Hint:
+
+You’ll need two bindings to track your progress. To know whether to put a space or a hash sign at a given position, you could test whether the sum of the two counters is even (% 2).
+
+Terminating a line by adding a newline character must happen after the line has been built up, so do this after the inner loop but inside the outer loop.
+
+*/
+
+let size = 8;{
+let board = "";
+for (let y = 0; y < size; y++){
+    for (let x = 0; x < size; x++){
+        if ((x + y) % 2 == 0){
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+    board += "\n";
+}
+console.log(board);
