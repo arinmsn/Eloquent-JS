c80a846fe036f72bb26e1a8e4bb6cf2ba62225a6
arinmsn <arinmsn@gmail.com>
2020-02-18 13:25:19 -0800

    3 more tests for 4.1 exersise

--- __tests__/4.1-sumOfRange.test.js
+++ __tests__/4.1-sumOfRange.test.js
@@ -5,3 +5,19 @@ describe("return an array containing the numbers from start to end", () => {
     expect(range(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 });
+
+describe("return range of numbers from start to end, utilizing 3rd optional argument, step", () => {
+  it("array = [5, 4, 3, 2], step = -1", () => {
+    expect(range(5, 2, -1)).toEqual([5, 4, 3, 2]);
+  });
+
+  it("array = [3,5,7,9], step = 2", () => {
+    expect(range(3, 10, 2)).toEqual([3, 5, 7, 9]);
+  });
+});
+
+describe("return the sum of the numbers in array", () => {
+  it("array = [1,2,3,4,5,6,7,8,9,10]", () => {
+    expect(sum(range(1, 10))).toEqual(55);
+  });
+});
