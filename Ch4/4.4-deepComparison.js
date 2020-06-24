8e031911f237c10333dc1dc746ac52c794df0172
arinmsn <arinmsn@gmail.com>
2020-06-23 19:37:59 -0700

    4.4 - Deep Comparison exercise

--- Ch4/4.4-deepComparison.js
+++ Ch4/4.4-deepComparison.js
@@ -1,24 +1,27 @@
 function deepEqual(a, b) {
-    if (a === b) return true;
-    
-    if (a == null || typeof a != "object" ||
-        b == null || typeof b != "object") return false;
-  
-    let keysA = Object.keys(a), keysB = Object.keys(b);
-  
-    if (keysA.length != keysB.length) return false;
-  
-    for (let key of keysA) {
-      if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
-    }
-  
-    return true;
+  if (a === b) return true;
+
+  if (a == null || typeof a != "object" || b == null || typeof b != "object")
+    return false;
+
+  let keysA = Object.keys(a),
+    keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
   }
-  
-  let obj = {here: {is: "an"}, object: 2};
-  console.log(deepEqual(obj, obj));
-  // → true
-  console.log(deepEqual(obj, {here: 1, object: 2}));
-  // → false
-  console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
-  // → true
+
+  return true;
+}
+
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+// → false
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+// → true
+
+module.exports = { deepEqual };
