79cb3a7cd4e82782d18a6a05088ea1dca4e6c4ed
arinmsn <arinmsn@gmail.com>
2020-06-23 19:37:25 -0700

    2.3 - Chessboard exercise

--- Ch2/2.3-Chessboard.js
+++ Ch2/2.3-Chessboard.js
@@ -29,16 +29,16 @@ Terminating a line by adding a newline character must happen after the line has 
 
 */
 
-let size = 8;{
+let size = 8;
 let board = "";
-for (let y = 0; y < size; y++){
-    for (let x = 0; x < size; x++){
-        if ((x + y) % 2 == 0){
-            board += " ";
-        } else {
-            board += "#";
-        }
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
     }
-    board += "\n";
+  }
+  board += "\n";
 }
 console.log(board);
