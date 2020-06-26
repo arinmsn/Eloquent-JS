fc7896b8a53c90f73792a07ec38f562c5c63f003
arinmsn <arinmsn@gmail.com>
2020-06-25 17:22:37 -0700

    5.3 Everything exercise (finding range of numbers in an array)

---
+++ Ch5/5.3-Everything.js
@@ -0,0 +1,24 @@
+/*
+
+Analogous to the some method, arrays also have an every method. This one returns true when the given function returns true for every element in the array. In a way, some is a version of the || operator that acts on arrays, and every is like the && operator. Implement every as a function that takes an array and a predicate function as parameters. Write two versions, one using a loop and one using the some method.
+
+
+*/
+
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some((element) => !predicate(element));
+}
+
+console.log(every([1, 3, 5], (n) => n < 10));
+// → true
+console.log(every([2, 4, 16], (n) => n < 10));
+// → false
+console.log(every([], (n) => n < 10));
+// → true
